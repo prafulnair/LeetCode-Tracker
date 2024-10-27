d0d13627532036290ef48bab3be8d648bc9a41a7
Praful Nair <prafulworkspace@gmail.com>
2024-10-27 18:20:49 -0400

    Top K freq element --updated with the right code.

--- top_K_frequest_elements.py
+++ top_K_frequest_elements.py
@@ -38,7 +38,7 @@ Follow up: Your algorithm's time complexity must be better than O(n log n), wher
 
 import heapq 
 class Solution:
-    def topKFrequent(self, nums: list[int], k: int) -> list[int]:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
         heap = []
 
@@ -49,17 +49,17 @@ class Solution:
                 map[n] += 1
             else:
                 map[n] = 1
-        
-        for key, value in map.items():
-            element = [key, value]
-            heap.append(element)
+    
+        # create a max heap. 
+        heap = [(-value, key) for key, value in map.items()]
         print(heap)
         heapq.heapify(heap)
+        print(heap)
         result = []
 
         while (k!=0):
             ele = heapq.heappop(heap)
-            result.append(ele[0])
+            result.append(ele[1])
             k -= 1
 
         return result
