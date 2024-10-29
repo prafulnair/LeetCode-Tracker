66e2e08f71b1f7059324db8e22c0f9333ef01344
Praful Nair <prafulworkspace@gmail.com>
2024-10-28 20:40:09 -0400

    Graph: Largest Connected Component

--- Graphs/largest_components.py
+++ Graphs/largest_components.py
@@ -1,3 +1,42 @@
+"""
+Write a function, largestComponent, that takes in the adjacency list of an undirected graph.
+The function should return the size (or the component itself) of the largest connected component in the graph 
+""" 
 
+def dfs(node, graph, seen, string):
+    if node in seen:
+        return
+    
+    
+    seen.add(node)
+    string = string + str(node) + "--"
+    for neighbor in graph[node]:
+        if neighbor not in seen:
+            string = dfs(neighbor, graph, seen, string)
+    
+    return string
 
-graph = 
+def largest_Component(graph):
+    component = []
+    seen = set()
+
+    for node in graph:
+        if node not in seen:
+            
+            string = dfs(node, graph, seen, "")
+            component.append(string)
+
+    return max(component, key=len)
+    
+
+graph = {
+    0: [8, 1, 5],
+    1: [0],
+    5: [0, 8],
+    8: [0, 5],
+    2: [3, 5],
+    3: [2, 4],
+    4: [3 ,2]
+}
+
+print(largest_Component(graph))
